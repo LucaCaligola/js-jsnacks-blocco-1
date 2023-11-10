dba22af9cb61ec21f02294dae92d15f23b7052ca
LucaCaligola <luca.caligola@gmail.com>
2023-11-10 11:52:42 +0100

    working on a new index.html and script.js

---
+++ snack-1/script.js
@@ -0,0 +1,9 @@
+// chiedere all'utente con due prompt gli anni di due persone 
+// e comunicare quale delle due è più grande
+
+const anniPrimaPersona = prompt ("qaunti anni hai?");
+const anniSecondaPersona = prompt ("qaunti anni hai?");
+
+if anniPrimaPersona > anniSecondaPersona {
+    console.log 
+}
