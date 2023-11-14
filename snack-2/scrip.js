ceeee956068472678f4a5aa66fbd233a3ea7c1b5
LucaCaligola <luca.caligola@gmail.com>
2023-11-14 12:18:38 +0100

    working on the for

--- snack-2/scrip.js
+++ snack-2/scrip.js
@@ -1,6 +1,7 @@
 // Scrivo in pagina i primi 6 numeri, scrivendo i numeri pari verdi. BN: numeri dispari gialli
-
+const wrapper = document.querySelector("div.wrapper")
 
 for (let i = 0; i < 6; i++) {
-    consoleLog ('i')
+    const element = `<p> ${i} </p>`;
+    wrapper.innerHTML += element;
 }
