31af3259474684b2fedaab39c4b4dc2a64178193
LucaCaligola <luca.caligola@gmail.com>
2023-11-14 12:19:53 +0100

    number 1 to 6 done

--- snack-2/scrip.js
+++ snack-2/scrip.js
@@ -1,7 +1,7 @@
 // Scrivo in pagina i primi 6 numeri, scrivendo i numeri pari verdi. BN: numeri dispari gialli
 const wrapper = document.querySelector("div.wrapper")
 
-for (let i = 0; i < 6; i++) {
+for (let i = 1; i <= 6; i++) {
     const element = `<p> ${i} </p>`;
     wrapper.innerHTML += element;
 }
