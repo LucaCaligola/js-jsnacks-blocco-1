71538140d9eda9a0a52bbb6ae718dbb8451df537
LucaCaligola <luca.caligola@gmail.com>
2023-11-15 12:11:01 +0100

    array done

--- snack-4/script.js
+++ snack-4/script.js
@@ -1,1 +1,17 @@
 // Ciclare per i primi 100 numeri e inserire in un array esclusivamente i numeri divisibili per 7 o per 8.
+let array = []
+for (let i = 1; i < 101; i++) {
+
+    if (i % 7 == 0) {
+        array.push(i)
+        console.log (array)
+    }
+
+    else if (i % 8 == 0) {
+        array.push(i)
+        console.log (array)
+    }
+
+    
+
+}
