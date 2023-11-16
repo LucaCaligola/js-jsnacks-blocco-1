90f1998719b0c2b70c08e3beedb18bc27160d974
LucaCaligola <luca.caligola@gmail.com>
2023-11-16 15:50:59 +0100

    working on the snack A

---
+++ snack-7/script.js
@@ -0,0 +1,17 @@
+// Crea un array vuoto. Chiedi per 6 volte all'utente di inserire un numero, se è dispari inseriscilo nell'array.
+
+let number = prompt("please enter a number")
+
+let dispari = false
+
+
+for (let i = 1; i < 7; i++) {
+
+    let array = []
+    
+    if (number % 2 != 0) {
+        let array =+ number
+        console.log(array)
+        
+    }
+}
